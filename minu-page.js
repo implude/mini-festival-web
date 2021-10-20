3b8f3e81de9524b71c5dd526abe3a104fc1abde3
정한 <35295182+rycont@users.noreply.github.com>
2021-10-20 23:42:08 +0000

    Add Autoscroll

--- minu-page.js
+++ minu-page.js
@@ -1,4 +1,12 @@
-var myFullpage = new fullpage('#fullpage', {
-    lockAnchors: true,
-    scrollingSpeed: 600,
+window.d = 7000;
+
+var myFullpage = new fullpage("#fullpage", {
+  lockAnchors: true,
+  scrollingSpeed: 600,
+  afterRender: () => {
+    setInterval(() => {
+      if (myFullpage.getActiveSection().isLast) myFullpage.moveTo(1);
+      myFullpage.moveSectionDown();
+    }, window.d);
+  },
 });
