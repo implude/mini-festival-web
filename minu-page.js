ddb720cb312c382d1584728258c8c4e504c0eda5
민유 <me@minyou.us>
2021-10-21 10:13:40 +0900

    아 속도 조절이 저게 아니었구나 이제는 제대로 작동할 거임

--- minu-page.js
+++ minu-page.js
@@ -1,8 +1,8 @@
-window.d = 7000;
+window.d = 5000;
 
 var myFullpage = new fullpage("#fullpage", {
   lockAnchors: true,
-  scrollingSpeed: 450,
+  scrollingSpeed: 600,
   afterRender: () => {
     setInterval(() => {
       if (myFullpage.getActiveSection().isLast) myFullpage.moveTo(1);
