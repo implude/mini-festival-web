3b888f942202c7cec0cf54cd1b2d6043662f3545
민유 <me@minyou.us>
2021-10-21 10:15:18 +0900

    Update minu-page.js

--- minu-page.js
+++ minu-page.js
@@ -2,7 +2,7 @@ window.d = 5000;
 
 var myFullpage = new fullpage("#fullpage", {
   lockAnchors: true,
-  scrollingSpeed: 600,
+  scrollingSpeed: 400,
   afterRender: () => {
     setInterval(() => {
       if (myFullpage.getActiveSection().isLast) myFullpage.moveTo(1);
