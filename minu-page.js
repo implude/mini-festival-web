586a5ad9f89fa5e6a16cbf857de1da5a58e41d93
민유 <me@minyou.us>
2021-10-21 09:08:03 +0900

    너무 느려서 속도를 좀 빠르게 했어요

--- minu-page.js
+++ minu-page.js
@@ -2,10 +2,10 @@ window.d = 7000;
 
 var myFullpage = new fullpage("#fullpage", {
   lockAnchors: true,
-  scrollingSpeed: 600,
+  scrollingSpeed: 500,
   afterRender: () => {
     setInterval(() => {
-      if (myFullpage.getActiveSection().isLast) myFullpage.moveTo(0);
+      if (myFullpage.getActiveSection().isLast) myFullpage.moveTo(1);
       myFullpage.moveSectionDown();
     }, window.d);
   },
